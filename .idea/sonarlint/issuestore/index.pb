4adf49f7135cb94b4bdc810d758d7142616bf8ef
markus walker <markus.walker@student.howest.be>
2021-11-28 22:34:07 +0100

    added login and register functionality

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Y
+)simon-greene-next/styles/About.module.css,0\5\058c950d0e621c0ddc99f05710c65a0f40972aeb
---
+++ simon-greene-next/pages/register.jsx
@@ -0,0 +1,9 @@
+import RegisterComponent from "../components/registerComponent";
+import Header from "../components/Header";
+const Register = () => (
+    <div>
+        <Header />
+    <RegisterComponent />
+    </div>
+)
+export default Register
